3767eb2708d2feb30581550e0f643053d819318c
Noy <noyhillel@gmail.com>
2016-12-15 22:32:54 +0000

    should have done this the first time lol

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ func main() {
 
 // Creating our function, I like creating functions rather than doing it all in main. Sue me
 func arrangeScreenShots() {
-	// Getting our possible screenshots/images in this example, there are loads more, I know
-	var imageExtensionList = []string{"jpeg","jpg","png"}
+	// Getting our possible screenshots/images in this example, there are loads more, I know, you can add them
+	var imageExtensionList = []string{"jpeg","jpg","png", "gif"}
 	// Creating our variables for the files and the error in the '.' directory, which will always be the current directory
 	files, err := ioutil.ReadDir(".")
 	// Check if there are any errors, if so, let us know
@@ -25,11 +25,13 @@ func arrangeScreenShots() {
 	// Iterate through the files
 	for _, file := range files {
 		// Checks if the files end with .png, .jpg, .jpeg
-		if strings.HasSuffix(file.Name(), imageExtensionList[0]) || strings.HasSuffix(file.Name(),
-			imageExtensionList[1]) || strings.HasSuffix(file.Name(), imageExtensionList[2]) {
-			// This method moves the files into the new directory, 'Screenshots' and gives them their same name.
-			// You have to give it it's own name while moving, just how it works.
-			os.Rename(file.Name(), "Screenshots/" + file.Name())
+		for _, extensions := range imageExtensionList {
+			// Iterate through the list, idk why I didn't do this before lol
+			if strings.HasSuffix(file.Name(), extensions) {
+				// This method moves the files into the new directory, 'Screenshots' and gives them their same name.
+				// You have to give it it's own name while moving, just how it works.
+				os.Rename(file.Name(), "Screenshots/" + file.Name())
+			}
 		}
 	}
 	// Let us know when the process is done.
